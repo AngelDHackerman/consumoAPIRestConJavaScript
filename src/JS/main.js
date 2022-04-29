21dcf61a40703856a2c0b17c0ab0f514e6c071dc
angelDariaux <angeldariaux@gmail.com>
2022-04-28 20:40:00 -0600

    recargado automatico, agregando y eleminando de forma correcta los gatitos todo super bien al momento!

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,10 +2,22 @@
 
 // ? query parameter agregado:
 // ? api key: 2e0a73a6-4887-496f-bcb9-294a0e42b9d3
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=2e0a73a6-4887-496f-bcb9-294a0e42b9d3'    
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?search?limit=2&api_key=2e0a73a6-4887-496f-bcb9-294a0e42b9d3'
+
+      // ! llamando nuestras API y sus query parameters y endPoints
+
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=2e0a73a6-4887-496f-bcb9-294a0e42b9d3';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?search?limit=2&api_key=2e0a73a6-4887-496f-bcb9-294a0e42b9d3';
+
+      // ? API endPoint dinamico para hacer los deletes en favorites:
+
+const API_URL_FAVORITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?search?limit=2&api_key=2e0a73a6-4887-496f-bcb9-294a0e42b9d3`
+
+
+      // ! span para mensajes de error: 
 
 const spanError = document.querySelector('#error');
+
+      // ! cargando los gatos aleatorios
 
 const  loadRandomMichis = async () => { 
 
@@ -38,7 +50,7 @@ const res = await fetch(API_URL_RANDOM)   // * fetch devuelve una promesa, await
   console.log(data[0].url);
 }
 
-      // ! Aqui agregamos los catos a favoritos
+      // ! Aqui agregamos los catos a FAVORITES
 
 const  loadFavoritesMichis = async () => { 
   
@@ -50,34 +62,41 @@ const  loadFavoritesMichis = async () => {
     spanError.innerHTML = `hubo un error ${res.status} ${data.message}`
 
   } else { 
+    const section = document.querySelector('#favoritesMinchos');
+    section.innerHTML = "";
+    const h2 = document.createElement('h2');
+    const h2Text = document.createTextNode('Michis favoritos');
+    h2.appendChild(h2Text);
+    section.appendChild(h2);
+
     data.forEach(michi => {     // ? Estamos recorriendo el array de DATA y eso se queda guardado en michi
 
-      // * Seleccionando donde vamos a incrustar el codigo.
-
-      const section = document.querySelector('#favoritesMinchos');
 
       // * Creando los elementos desde JS 
 
       const article = document.createElement('article');
       const img = document.createElement('img');
       const btn = document.createElement('button');
+
+      // ? Agregando el boton para eliminar los gatitos:
+
       const btnText = document.createTextNode('Sacar al mich de favoritos');
 
       // * Formando nuestro html desde JS
 
-      btn.appendChild(btnText);
       img.src = michi.image.url;
       img.width = 150;
+      btn.appendChild(btnText);
+      btn.onclick = () => deleteFavoriteMichi(michi.id);      // todo: Aqui eliminamos con el boton al gatito en favoritos
       article.appendChild(img);
       article.appendChild(btn);
       section.appendChild(article);
 
     })
-  }
-  
+  } 
 }
 
-      // ! Asi se crea una solicitud de post:
+      // ! Asi se crea una solicitud de post, (para guardar en favoritos):
 
 const saveFavoriteMichi = async (id) => { 
   const res = await fetch(API_URL_FAVORITES, {   
@@ -97,9 +116,30 @@ const saveFavoriteMichi = async (id) => {
 
   if (res.status !== 200) { 
     spanError.innerHTML = `hubo un error ${res.status} ${data.message}`
+  } else { 
+    console.log('Michi guardado en favoritos')
+    loadFavoritesMichis();
   }
 }
 
+      // ! Eliminando los gatos de favoritos: 
+
+const deleteFavoriteMichi = async (id) => { 
+  const res = await fetch(API_URL_FAVORITES_DELETE(id), { 
+    method: 'DELETE',
+  });
+  const data = await res.json();
+
+  if (res.status !== 200) { 
+    spanError.innerHTML = `Hubo un error ${res.status} ${data.message}`;
+  } else { 
+    console.log(`Michi eliminado de favoritos`);
+    loadFavoritesMichis();
+  }
+}
+
+
+      // ! Llamando a las funciones para que se ejecuten al cargar la pagina.
 
 loadRandomMichis();
 loadFavoritesMichis();
